636ffae8aba53f75b0158b289747b733c766326c
Mansi Thakkar <mansithakkar2699@gmail.com>
2026-03-03 20:43:30 +0530

    product crud

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const dbConnection = require("./src/utils/DBConnection");
 dbConnection();
 
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -8,6 +8,45 @@ const getAllProducts = async (req,res) => {
     })
 }
 
+const getProductByID = async (req,res) => {
+    const foundProduct = await productSchema.findById(req.params.id)
+    if(foundProduct){
+        res.json({
+            message: "Product found",
+            data: foundProduct
+        })
+    }else {
+        res.json({
+            message: "Product not found"
+        })
+    }
+}
+
+const addProduct = async (req,res) => {
+    const createdProduct = await productSchema.create(req.body)
+    res.status(201).json({
+        message: "Product Created",
+        data: createdProduct
+    })
+}
+
+const deleteProduct = async (req,res) => {
+    const deletedProduct = await productSchema.findByIdAndDelete(req.params.id)
+    if(deletedProduct){
+        res.json({
+            message: "Product deleted",
+            data: deletedProduct
+        })
+    }else {
+        res.json({
+            message: "Product not found"
+        })
+    }
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductByID,
+    addProduct,
+    deleteProduct
 }
